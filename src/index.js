734b28a06071af62fa10fe654a85334a94ab02ad
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-02-08 11:02:38 -0500

    Sexy and I Know JS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,102 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+document.addEventListener('DOMContentLoaded', setupPage)
 
-  let imageId = 1 //Enter the id from the fetched image here
+let imageId = 1981 //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const imageCard = document.querySelector('#image_card')
+const commentContainer = document.querySelector('#comments')
+const likeBtn = document.querySelector('#like_button')
+const commentForm = document.querySelector('#comment_form')
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+let picture = document.querySelector('#image')
+let title = document.querySelector('#name')
+let likes = document.querySelector('#likes')
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+function setupPage() {
+  likeHandler()
+  formHandler()
+  renderImage()
+  renderComments()
+} 
 
-})
+function renderImage() {
+  getImage().then(imageView)
+} 
+
+function getImage() {
+  return fetch(imageURL).then(res => res.json())
+} 
+
+function imageView(image) {
+  picture.src = image.url 
+  title.textContent = image.name 
+  likes.textContent = image.like_count
+} 
+
+function renderComments() {
+  getImage().then(commentView)
+} 
+
+function commentView(image) {
+  commentContainer.textContent = ""
+  let allComments = image.comments 
+  // debugger
+  allComments.forEach(c => {
+    let comment = document.createElement('li')
+    comment.textContent = c.content
+    commentContainer.appendChild(comment)
+  })
+}
+
+function likeHandler() {
+  likeBtn.addEventListener('click', addLike)
+} 
+
+function addLike() {
+  let newLikes = parseInt(likes.textContent) + 1
+  likes.textContent = newLikes 
+
+  updateLikes()
+} 
+
+function updateLikes() {
+  return fetch(likeURL,{
+    method: 'POST',
+    headers: 
+    {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify({
+      image_id: imageId
+    })
+  })   
+} 
+
+function formHandler() {
+  commentForm.addEventListener('submit',newComment)
+} 
+
+function newComment() {
+  event.preventDefault()
+  let newContent = event.target.comment.value
+  createComment(newContent).then(renderComments)
+  event.target.reset()
+}
+
+function createComment(newContent) {
+  return fetch(commentsURL,{
+    method: 'POST',
+    headers: 
+    {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify({
+      image_id: imageId, 
+      content: newContent
+    })
+  })  
+}
